1576a7278200163a5473471b1ccafdc6a160ce14
py4524208-web <py4524208@gmail.com>
2026-07-30 02:10:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,72 +1,21 @@
-import { db } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
-import {
-  ref,
-  push,
-  set,
-  onValue,
-  remove
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
-
-const ordersRef = ref(db, "orders");
-
-window.addOrder = function () {
-
-  const user = prompt("Customer Name");
-  if (!user) return;
-
-  const service = prompt("Service Name");
-  if (!service) return;
-
-  const quantity = prompt("Quantity");
-  if (!quantity) return;
-
-  const price = prompt("Price");
-  if (!price) return;
-
-  const newRef = push(ordersRef);
-
-  set(newRef, {
-    user: user,
-    service: service,
-    quantity: quantity,
-    price: price,
-    status: "Pending"
-  });
-
+const firebaseConfig = {
+  apiKey: "AIzaSyACKYxVzoamyXcczkrkRHV-lQ26DSInHgs",
+  authDomain: "nowasmm.firebaseapp.com",
+  databaseURL: "https://nowasmm-default-rtdb.firebaseio.com",
+  projectId: "nowasmm",
+  storageBucket: "nowasmm.firebasestorage.app",
+  messagingSenderId: "103011223164",
+  appId: "1:103011223164:web:80ed5d53abd26ca5026bf9",
+  measurementId: "G-0JTHRZMQSX"
 };
 
-onValue(ordersRef, (snapshot) => {
+const app = initializeApp(firebaseConfig);
 
-  const tbody = document.querySelector("#ordersTable tbody");
-  tbody.innerHTML = "";
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-  snapshot.forEach((item) => {
-
-    const order = item.val();
-
-    tbody.innerHTML += `
-      <tr>
-        <td>${item.key}</td>
-        <td>${order.user}</td>
-        <td>${order.service}</td>
-        <td>${order.quantity}</td>
-        <td>${order.price}</td>
-        <td>${order.status}</td>
-        <td>
-          <button onclick="deleteOrder('${item.key}')">Delete</button>
-        </td>
-      </tr>
-    `;
-
-  });
-
-});
-
-window.deleteOrder = function(id) {
-
-  if (confirm("Delete this order?")) {
-    remove(ref(db, "orders/" + id));
-  }
-
-};
+export { app, auth, db };
