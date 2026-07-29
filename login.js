61fc94a5a5727c8912f87ac17e1ffe5bc43c5ccb
py4524208-web <py4524208@gmail.com>
2026-07-29 22:40:59 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,29 @@
+import { app } from "./firebase.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
+window.login = function () {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+        alert("Login Successful");
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+        alert(error.code + "\n" + error.message);
+    });
+
+};
