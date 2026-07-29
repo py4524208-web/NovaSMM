cbdeae71de3a60f549801a2697c8b062234517a9
py4524208-web <py4524208@gmail.com>
2026-07-29 23:48:00 +0530

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,67 +1,71 @@
 import { db } from "./firebase.js";
+
 import {
-getDatabase,
-ref,
-push,
-set,
-onValue,
-remove
+  ref,
+  push,
+  set,
+  onValue,
+  remove
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
-window.addOrder=function(){
+const ordersRef = ref(db, "orders");
 
-const user=prompt("User Name");
-const service=prompt("Service");
-const qty=prompt("Quantity");
-const price=prompt("Price");
+window.addOrder = function () {
 
-if(!user)return;
+    const user = prompt("Customer Name");
+    if (!user) return;
 
-const id=Date.now();
+    const service = prompt("Service Name");
+    if (!service) return;
 
-set(ref(db,"orders/"+id),{
+    const quantity = prompt("Quantity");
+    if (!quantity) return;
 
-id,
-user,
-service,
-qty,
-price,
-status:"Pending"
+    const price = prompt("Price");
+    if (!price) return;
+
+    const newRef = push(ordersRef);
+
+    set(newRef, {
+        orderId: newRef.key,
+        user,
+        service,
+        quantity,
+        price,
+        status: "Pending"
+    });
+
+};
+
+onValue(ordersRef, (snapshot) => {
+
+    const tbody = document.querySelector("#ordersTable tbody");
+    tbody.innerHTML = "";
+
+    snapshot.forEach((item) => {
+
+        const order = item.val();
+
+        tbody.innerHTML += `
+        <tr>
+            <td>${order.orderId}</td>
+            <td>${order.user}</td>
+            <td>${order.service}</td>
+            <td>${order.quantity}</td>
+            <td>${order.price}</td>
+            <td>${order.status}</td>
+            <td>
+                <button onclick="deleteOrder('${order.orderId}')">Delete</button>
+            </td>
+        </tr>
+        `;
+
+    });
 
 });
 
-}
+window.deleteOrder = function(id) {
 
-const tbody=document.querySelector("#ordersTable tbody");
+    remove(ref(db, "orders/" + id));
 
-onValue(ref(db,"orders"),snapshot=>{
-
-tbody.innerHTML="";
-
-snapshot.forEach(data=>{
-
-const o=data.val();
-
-tbody.innerHTML+=`
-<tr>
-<td>${o.id}</td>
-<td>${o.user}</td>
-<td>${o.service}</td>
-<td>${o.qty}</td>
-<td>$${o.price}</td>
-<td>${o.status}</td>
-<td>
-<button onclick="deleteOrder('${o.id}')">Delete</button>
-</td>
-</tr>
-`;
-
-});
-
-});
-
-window.deleteOrder=function(id){
-
-remove(ref(db,"orders/"+id));
-
-}
+};
