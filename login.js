7e945e1719514aaecb483783986e599214bf4abe
py4524208-web <py4524208@gmail.com>
2026-07-30 12:16:17 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,46 +1,5 @@
-import { app } from "./firebase.js";
+alert("JS Loaded");
 
-import {
-  getAuth,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-
-alert("login.js loaded");
-
-const auth = getAuth(app);
-
-window.login = async function () {
-
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
-
-  if (!email || !password) {
-    alert("Please fill all fields");
-    return;
-  }
-
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-
-    console.log(userCredential.user);
-
-    alert("✅ Login Successful");
-
-    window.location.href = "./dashboard.html";
-
-  } catch (error) {
-
-    console.error(error);
-
-    alert(
-      "Error Code: " + error.code +
-      "\n\nMessage: " + error.message
-    );
-
-  }
-
-};
+document.getElementById("loginBtn").addEventListener("click", () => {
+    alert("Button Works");
+});
