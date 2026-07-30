b38fea522c8de84c0a3d094046cd2c78790178c1
py4524208-web <py4524208@gmail.com>
2026-07-31 00:38:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyACKYxVzoamyXcczkrkRHV-lQ26DSInHgs",
-  authDomain: "nowasmm.firebaseapp.com",
-  databaseURL: "https://nowasmm-default-rtdb.firebaseio.com",
-  projectId: "nowasmm",
-  storageBucket: "nowasmm.firebasestorage.app",
-  messagingSenderId: "103011223164",
-  appId: "1:103011223164:web:80ed5d53abd26ca5026bf9",
-  measurementId: "G-0JTHRZMQSX"
+export const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getDatabase(app);
 
-const auth = getAuth(app);
-
-export {
-  firebaseConfig,
-  app,
-  db,
-  auth
-};
+export { app, db };
